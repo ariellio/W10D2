d063d9c7e391cdc62eac021cd26881480b97252d
shiguang <huangs2022672@gmail.com>
2021-06-23 15:45:31 -0400

    Completed todo_list_container.jsx working on todo_list.jsx

---
+++ frontend/components/todos/todo_list.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+
+export default (props) => {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+
+    }
+  }
+
+  this.setState
+
+  debugger
+  return (
+    
+    <>
+    
+    <h3>Todo List goes here!</h3>
+
+    </>
+  )
+}
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from './components/test.jsx';
+import Root from './components/root.jsx';
 import configureStore from './store/store.js';
 import { receiveTodo, receiveTodos, removeTodo } from './actions/todo_actions.js';
 import { receiveStep, receiveSteps, removeStep } from './actions/step_actions.js';
-
-
+import { allTodos } from './reducers/selectors'
 
 document.addEventListener("DOMContentLoaded", () =>  {
     window.store = configureStore();
@@ -15,7 +14,8 @@ document.addEventListener("DOMContentLoaded", () =>  {
     window.receiveStep = receiveStep;
     window.receiveSteps = receiveSteps;
     window.removeStep = removeStep;
+    window.allTodos = allTodos;
     const content = document.getElementById("content");
-    ReactDOM.render(<Test />, content);
+    ReactDOM.render(<Root store={store} />, content);
 });
 
